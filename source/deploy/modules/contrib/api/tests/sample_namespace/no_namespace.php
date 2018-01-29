c042b1c8c1070286589684f4d485a67734cca22c
Dinesh Waghmare <rdn.v03@gmail.com>
2018-01-29 18:28:46 +0530

    Added Libraries and Modules required for API Portal Demo

---
+++ source/deploy/modules/contrib/api/tests/sample_namespace/no_namespace.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file No namespace here.
+ */
+
+/**
+ * Sample class not in a namespace.
+ */
+class ClassQ {
+  /**
+   * A method.
+   */
+  function bMethod() {
+  }
+}
+
+/**
+ * And a function name.
+ */
+function another_function() {
+}
